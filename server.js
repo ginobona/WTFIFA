fb2eea23080262f1bed0f31f75ca4cd8cec8dfc0
Stephen Salzer <stephen.salzer@gmail.com>
2015-06-05 10:06:49 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/secret', function(req, res) {
+  res.send(process.env.SECRET);
+});
+
+app.use(function (req, res) {
+  var options = {
+    root: __dirname + '/public/'
+  };
+  res.sendFile('404.html', options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log('Sent:', '404.html ' + new Date());
+    }
+  });
+})
+
+var server = app.listen(process.env.PORT || 5000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('I am listening at http:// ', host, port);
+});
